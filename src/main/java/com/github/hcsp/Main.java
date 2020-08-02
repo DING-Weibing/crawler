bbc60f54603a22a479bfe7aac16b471763a8f38b
DING Weibing <dingweibing@outlook.jp>
2020-08-02 21:15:57 +0900

    基本代码

---
+++ src/main/java/com/github/hcsp/Main.java
@@ -0,0 +1,18 @@
+package com.github.hcsp;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
+        Elements newsHeadlines = doc.select("#mp-itn b a");
+        for (Element headline : newsHeadlines) {
+            System.out.println(headline.attr("title"));
+        }
+    }
+}
